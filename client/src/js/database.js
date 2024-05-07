43777e964af1b24ef22a6b93b8970866d2af8e77
Nico P <nicopetkas@gmail.com>
2024-05-07 16:17:36 -0700

    code update

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,6 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -15,12 +14,12 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => { console.error('putDb not implemented');
-  const db = await openDB('jate', 1);
-  const action = db.transaction('jate', 'readwrite');
-  const store = action.objectStore('jate');
-  const request = store.put({ id: 1, value: content});
-  const result = await request;
-  console.log('Content saved to database', result);
+const db = await openDB('jate', 1);
+const action = db.transaction('jate', 'readwrite');
+const store = action.objectStore('jate');
+const request = store.put({ id: 1, value: content});
+const result = await request;
+console.log('Content saved to database', result);
 }
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => { console.error('getDb not implemented');
@@ -32,5 +31,4 @@ const result = await request;
 console.log('result.value', result);
 return result?.value;
 }
-
 initdb();
